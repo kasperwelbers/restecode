438ec2e3ca3c328d4632ba2b686f598745116b7d
kasperwelbers <kasperwelbers@gmail.com>
2019-11-28 16:04:58 +0100

    fixed gtd_event_coverage

--- R/gtd_match_analysis.r
+++ R/gtd_match_analysis.r
@@ -14,17 +14,15 @@ gtd_event_coverage <- function(g, weight_thres, use_geomatch) {
 
   meta = subset(g$from_meta, .complete_window)
   e = subset(g$d, .use)
-  e = merge(e, g$to_meta[,c('document_id','terrorism')], by.x='to', by.y='document_id', all.x=T)
-  e = e[, list(N_news=length(weight), has_news=T, terrorism_news=sum(terrorism)), by='from']
+  e = merge(e, g$to_meta[,c('document_id')], by.x='to', by.y='document_id', all.x=T)
+  e = e[, list(N_news=length(weight), has_news=T), by='from']
 
   #meta$geo_distance = london_dist_km(meta)
   e = merge(meta, e, by.x='document_id', by.y='from', all=T)
   e$N_news[is.na(e$N_news)] = 0
   e$has_news[is.na(e$has_news)] = 0
-  e$terrorism_news[is.na(e$terrorism_news)] = 0
 
   e$killed[is.na(e$killed)] = 0
   e$wounded[is.na(e$wounded)] = 0
-
   e
 }
